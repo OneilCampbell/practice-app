16d9c07578b2871819f1091aa5547f723f639bb3
oneil <oneilcampbell19@gmail.com>
2019-07-25 19:15:14 -0400

    implemented logic and view for display component

--- src/app/display/display.component.ts
+++ src/app/display/display.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PracticeDataService } from '../services/practice-data.service';
 
 @Component({
   selector: 'app-display',
@@ -6,10 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./display.component.css']
 })
 export class DisplayComponent implements OnInit {
+  // TODO: specify type of dataToDisplay
+  dataToDisplay;
 
-  constructor() { }
+  constructor(
+    private dataService: PracticeDataService
+  ) { }
 
   ngOnInit() {
+    this.getDataToDisplay()
   }
 
+  getDataToDisplay(): void {
+    this.dataService.getData().subscribe(
+      data => this.dataToDisplay = data
+    );
+  }
+
+  // TODO: implement
+  updateData(data): void {
+
+  }
+
+  createData(data): void {
+    this.dataService.createData(data).subscribe()
+  }
+
+  deleteData(id: number): void {
+    this.dataService.deleteData(id).subscribe()
+  }
 }
