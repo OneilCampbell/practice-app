a0c3e2da1073dc89da336c867d4db698a6c0f386
oneil <oneilcampbell19@gmail.com>
2019-07-30 18:52:44 -0400

    imported forms module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './modules/app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app-component/app.component';
 import { LoginComponent } from './login/login.component';
@@ -18,7 +19,8 @@ import { ChatService } from './services/chat/chat.service';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [ChatService],
   bootstrap: [AppComponent]
